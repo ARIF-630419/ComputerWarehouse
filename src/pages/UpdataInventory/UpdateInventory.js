163763a8b3dfe4d03c0a7ff42f65dde71279c6d3
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-19 00:35:39 +0600

    delivered and add quantity added

--- src/pages/UpdataInventory/UpdateInventory.js
+++ src/pages/UpdataInventory/UpdateInventory.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const UpdateInventory = () => {
@@ -10,7 +11,18 @@ const UpdateInventory = () => {
             .then(res => res.json())
             .then(data => setInventory(data))
     }, [])
+    const increaseQuantity = quantity => {
+        const newQuantity = quantity - 1;
+        console.log(newQuantity);
+        inventory.quantity = newQuantity;
 
+    }
+    const handelQuantity = event => {
+        event.preventDefault();
+        const addQuantity = event.target.quantity.value;
+        console.log(addQuantity);
+
+    }
     return (
         <div className='container mt-5'>
             <div className="card" style={{ width: "65rem" }}>
@@ -22,6 +34,17 @@ const UpdateInventory = () => {
                     <h5 className="card-title">Supplier Name :{inventory.supplierName}</h5>
                     <p className="card-text">Description : {inventory.description}</p>
                 </div>
+            </div>
+            <div className='m-5'>
+                <button onClick={() => increaseQuantity(inventory.quantity)} className='btn btn-primary d-block w-50 mx-auto mb-2'>Delivered</button>
+            </div>
+            <div className=' container w-50 mx-auto'>
+                <h2 className=' text-primary text-center mt-2'>Please add quantity.</h2>
+                <form onSubmit={handelQuantity}>
+                    <input className='d-block w-50 mx-auto m-2' type="number" name="quantity" placeholder='Enter quantity.' required />
+                    <br />
+                    <input className='d-block w-50 mx-auto' type="submit" value="Add Quantity" />
+                </form>
             </div>
         </div>
     );
