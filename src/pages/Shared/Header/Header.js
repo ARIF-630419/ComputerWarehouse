1f20c30e3f4824a3bbbde642f5e55ad9ae1b0c44
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-18 22:52:48 +0600

    some update

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -19,7 +19,6 @@ const Header = () => {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto" >
                             <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
                             <Nav.Link as={Link} to="/blogs">blogs</Nav.Link>
                         </Nav>
                         <Nav>
