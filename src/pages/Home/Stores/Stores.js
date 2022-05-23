df9b65b10fd1a40059edef0843111ef8f67f002f
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-23 10:47:21 +0600

    home page maximum 6 items show added

--- src/pages/Home/Stores/Stores.js
+++ src/pages/Home/Stores/Stores.js
@@ -8,12 +8,13 @@ const Stores = () => {
             .then(res => res.json())
             .then(data => setStores(data))
     }, []);
+    const sixStores = stores.slice(0, 6);
     return (
         <div className='container mt-5'>
             <h2 className=' text-primary text-center'>In Stock</h2>
             <div className='row'>
                 {
-                    stores.map(store => <Store
+                    sixStores.map(store => <Store
                         key={store._id}
                         store={store}
                     ></Store>)
