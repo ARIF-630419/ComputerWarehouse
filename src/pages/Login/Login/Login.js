c42c4e6cbf0b56862c340e11c7640acd00d5e233
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-24 01:38:40 +0600

    jwt added

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -28,20 +29,19 @@ const Login = () => {
         return <Loading></Loading>
     }
 
-    if (user) {
-        navigate(from, { replace: true });
-    }
-
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const navigateRegister = event => {
--- src/pages/MyItem/MyItem.js
+++ src/pages/MyItem/MyItem.js
@@ -4,18 +4,28 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Item from '../../Item/Item';
 import axiosPrivate from '../../api/axiosPrivate';
+import { useNavigate } from 'react-router-dom';
 
 const MyItem = () => {
     const [user] = useAuthState(auth);
     const [items, setItems] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         const getItems = async () => {
             const email = user.email;
             console.log('email', email);
             const url = `http://localhost:5000/MyItems?email=${email}`;
-            const { data } = await axiosPrivate.get(url);
-            console.log("data", data);
-            setItems(data);
+            try {
+                const { data } = await axiosPrivate.get(url);
+                setItems(data);
+            }
+            catch (error) {
+                console.log(error.message);
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login')
+                }
+            }
 
         }
         getItems();
